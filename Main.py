2ad4f9982963a538745ca9ed8ed92a902b5d460f
maya-harel <mayaharel28@gmail.com>
2018-11-19 17:34:22 +0200

    initial commit

---
+++ Main.py
@@ -0,0 +1,9 @@
+
+def main():
+    print "Hello it is I"
+
+    # get input. parse into Constrain OBJ
+    # create algorithm that gets a Constrain OBJ and parses according to it
+
+if __name__ == "__main__":
+    main()
