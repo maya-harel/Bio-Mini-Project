ab490d878383ed8fa34c1d33dac87f874c55f6a7
maya-harel <mayaharel28@gmail.com>
2019-01-31 13:41:44 +0200

    31/1 maya

--- FPtree.py
+++ FPtree.py
@@ -21,41 +21,35 @@ class treeNode:
             child.disp(ind+1)
 
 
-# for each cog, count how many different genomes it appears in AND parse according to COG
-def countOccurence(genomeDat, minSup):
-    # counterTable = {}
-    headerTable = {}
-    counterTable = defaultdict(list)
-    for item in genomeDat: # item is a tuple - (genome, COGList)
-        for cog in item[1]:
-            genome = item[0]
-            if genome not in counterTable[cog]:
-                counterTable[cog].append(genome)
-                continue
-
-    # for item in genomeDat:
-    #     genome = item[0]
-    #     for cog in item[1]:
-    #         # counterTable[cog] is a list
-    #         if genome not in counterTable[cog]:
-    #             counterTable[cog].append(genome)
+# for each cog, count how many different genomes it appears in
+def countOccurence(cogsDict, dataSet, minSup):
+    headerTable = {} # key = cog, value = num of different genomes
+    counterTable = defaultdict(set) # key = cog, value = list of genomes
+    counter = 0
+    for item in dataSet: # item is a list of COGs
+        cogString = "-".join(item)
+        if cogString not in cogsDict.keys():
+            print "SHIT the string is not here " + cogString
+            continue
+        genomeList = cogsDict[cogString]
+        for cog in item:
+            for genome in genomeList:
+                counterTable[cog].add(genome)
 
     for cog in counterTable.keys():
-        if len(counterTable[cog]) < minSup:
-            del (counterTable[cog])
-            # continue
-        headerTable[cog] = len(counterTable[cog])
+        if len(counterTable[cog]) >= minSup:
+            headerTable[cog] = len(counterTable[cog])
+
+    print "CURRENT HEADER TABLE " + str(headerTable)
 
     return headerTable
 
 
-
 # create FP-tree from dataset
-def createTree(genomeDat, dataSet, minSup):  # currCog is what we are looking for in the current iteration
-    logging.info("creating FP tree")
+def createTree(cogsDict, dataSet, minSup):  # currCog is what we are looking for in the current iteration
     headerTable = {}
     # go over dataSet twice
-    headerTable = countOccurence(genomeDat, minSup)
+    headerTable = countOccurence(cogsDict, dataSet, minSup)
     # for trans in dataSet:  # first pass counts frequency of occurance
     #     for item in trans:
     #         headerTable[item] = headerTable.get(item, 0) + dataSet[trans]
@@ -63,17 +57,17 @@ def createTree(genomeDat, dataSet, minSup):  # currCog is what we are looking fo
     #     if headerTable[k] < minSup:
     #         del (headerTable[k])
     freqItemSet = set(headerTable.keys())
-    # logging.info("freqItemSet : " + str(freqItemSet))
-    # print 'freqItemSet: ', freqItemSet
+    logging.info("freqItemSet : " + str(freqItemSet))
+    print 'freqItemSet: ', freqItemSet
     if len(freqItemSet) == 0:
         logging.info("none of the items are frequent enough.")
         return None, None  # if no items meet min support -->get out
     for k in headerTable:
         headerTable[k] = [headerTable[k], None]  # reformat headerTable to use Node link
-    # logging.info("headerTable : " + str(headerTable))
-    # print 'headerTable: ', headerTable
+    logging.info("headerTable : " + str(headerTable))
+    print 'headerTable: ', headerTable
     retTree = treeNode('Null Set', 1, None)  # create tree
-    for tranSet, count in dataSet.items():  # go through dataset 2nd time #TODO
+    for tranSet, count in dataSet.items():  # go through dataset 2nd time
         localD = {}
         for item in tranSet:  # put transaction items in order
             if item in freqItemSet:
--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import logging
 import FPtree
 import sys
+from collections import defaultdict
 
 
 def removeVal(val, list):
@@ -9,66 +10,88 @@ def removeVal(val, list):
 
 # this function creates a list out of input data - a tuple for each line (genome number, list of cogs)
 def parseInput(file, window):
-    dictdata = []
+    data = [[]] # windows of cogs
+    counter = 0
+    cogsDict = defaultdict(set) # key = cog window, value = genome list
     with open(file) as inputFile:
         for line in inputFile:
             cogsTemp = line.split('#')[-1]
             genomeNum = cogsTemp.split('\t')[0]
             cogList = cogsTemp.split('\t')[1:]
+            cogList = removeVal('\n', cogList)
             if len(cogList) <= window:
                 tempCogs = removeVal('X', cogList)
-                tempCogs = removeVal('\n', tempCogs)
-                dictdata.append((genomeNum, tempCogs))
+
+                # sortedCogs = sorted(tempCogs)
+                data.append(sorted(tempCogs)) # add to data
+                cogString = "-".join(sorted(tempCogs))
+                cogsDict[cogString].add(genomeNum) # add to cog dictionary
+                # if counter < 100:
+                #     print str(tempCogs) + " ----- " + cogString
+                #     counter += 1
             else :
-                for i in range(0, len(cogList)-window+1):
-                    tempCogs = removeVal('X', cogList[i:i+window])
-                    tempCogs = removeVal('\n', tempCogs)
-                    dictdata.append((genomeNum, tempCogs))
+                for i in range(0, len(cogList)-window):
+                    tempCogs = removeVal('X', cogList[i:i+window-1])
 
-    # with open('testDataParsing.txt', 'w+') as testFile :
-    #     testFile.write(str(dictdata))
+                    # sortedCogs = sorted(tempCogs)
+                    data.append(sorted(tempCogs))  # add to data
+                    cogString = "-".join(sorted(tempCogs))
+                    cogsDict[cogString].add(genomeNum)  # add to cog dictionary
+                    # if counter < 100:
+                    #     print str(sortedCogs) + " ----- " + cogString
+                    #     counter += 1
 
-    return dictdata
+    # with open('testDataParsing.txt', 'w+') as file:
+    #     file.write(str(data))
+    #
+    # with open('testCOGdict.txt', 'w+') as file:
+    #     file.write(str(cogsDict))
+
+    return data, cogsDict
 
 
 def createInitSet(dataSet):
     retDict = {}
-    for item in dataSet:
-        trans = item[1]
+    for trans in dataSet:
         retDict[frozenset(trans)] = 1
     return retDict
 
 
 def main():
 
-    logging.basicConfig(filename='BioMiniProject.log', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S')
+    logging.basicConfig(filename='BioMiniProject.log', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S') #TODO
     logging.info('##################### Starting our program #####################')
 
+    # constraint = sorted(['1744', '3845', '4603', '1079'])
     constraint = ['1744', '3845', '4603', '1079']
-    minSup = 105
+    print constraint.sort()
+
+    return 
+    minSup = 50
     window = 7
 
     filePath = sys.argv[1]
-    genomeDat = parseInput(filePath, window)
-    dat = createInitSet(genomeDat)
+    data, cogsDict = parseInput(filePath, window)
+    dataSet = createInitSet(data)
 
-    myHeaderTab = {}
+
     for cog in constraint:
-        # logging.info('# current COG : ' + str(cog))
-        myFPtree, myHeaderTab = FPtree.createTree(genomeDat, dat, minSup)
+        print "current iteration - COG : " + str(cog)
+        myFPtree, myHeaderTab = FPtree.createTree(cogsDict, dataSet, minSup)
         # find sub tree for COG and this is the new data for the next iteration
 
-        with open('testHeaderTable.txt', 'w+') as file:
-            file.write(str(myHeaderTab))
+        # with open('testHeaderTable.txt', 'w+') as file:
+        #     file.write(str(myHeaderTab))
 
         if cog in myHeaderTab.keys():
             logging.info('# getting sub tree for current COG : ' + str(cog) + ' with frequency of ' + str(myHeaderTab[cog][0]))
-            dat = FPtree.findPrefixPath(cog, myHeaderTab[cog][1])
+            print '# getting sub tree for current COG : ' + str(cog) + ' with frequency of ' + str(myHeaderTab[cog][0])
+            dataSet = FPtree.findPrefixPath(cog, myHeaderTab[cog][1])
         else :
             print 'Huston we have a problem'
             return
 
-    print "RESULTS " #TODO - what are the results !?!?
+    print "RESULTS " + str(dataSet)
 
 
 if __name__ == "__main__":
